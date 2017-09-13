43f2d0a593e5e432a0ce614ecf382c0065372480
Daniel Trnka <daniel.trnka@gmail.com>
2017-09-14 00:59:02 +0200

    Added more testing tasks with parameters

--- main.c
+++ main.c
@@ -16,21 +16,23 @@ volatile unsigned int veces;
 void task1(void* param) {
 	int i = 0;
 	for(;;) {
-		log_msg("TICK: %d\n", i++);
+		log_msg("TICK[%d]: %d\n",  (int) param, i++);
 		taskYIELD();
-		//vTaskDelay(1000);
+//		vTaskDelay(1000);
 	}
 }
 
-void kernel_main() {
-	portDISABLE_INTERRUPTS();
+void task2(void* param) {
+	int i = 0;
+	for(;;) {
+		log_msg("Different code[%d]: %d\n",  (int) param, i++);
+		taskYIELD();
+//		vTaskDelay(1000);
+	}
+}
 
-	log_msg("initialized now\n");
-	int* mem = pvPortMalloc( 16 );
-	log_msg("allocated\n");
-
-	log_msg("CREATING TASK\n");
-	BaseType_t msg = xTaskCreate(task1, "LED_0", 128, NULL, 0, NULL);
+void create_task(TaskFunction_t fn, const char* name, void* param) {
+	BaseType_t msg = xTaskCreate(fn, name, 128, param, 0, 0);
 	if(msg != pdPASS) {
 		if(msg == errCOULD_NOT_ALLOCATE_REQUIRED_MEMORY) {
 			log_msg("xtaskcreate failed: memory\n");
@@ -39,11 +41,18 @@ void kernel_main() {
 		}
 		for(;;) {}
 	}
-	log_msg("created task\n");
+}
+
+void kernel_main() {
+	portDISABLE_INTERRUPTS();
+
+	create_task(task1, "TASK_1", 22);
+	create_task(task1, "TASK_2", 44);
+	create_task(task2, "TASK_3", 66);
 
 	vTaskStartScheduler();
 
-	log_msg("We are lost\n");
+	log_msg("PANIC: scheduler stopped\n");
 	for(;;) {}
 
 
