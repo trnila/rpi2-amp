853c5bb62e49285dc6bec99feec43db118190575
Daniel Trnka <daniel.trnka@gmail.com>
2017-10-14 20:10:04 +0200

    log.sh: obtain log address in each cycle, because address can relocate when new code is loaded

--- log.sh
+++ log.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-log_addr=$(objdump -t app.elf  | grep _log | cut -f 1 -d ' ')
-
 while true; do
+    log_addr=$(objdump -t app.elf  | grep _log | cut -f 1 -d ' ')
 	clear
 	./tools/logcat "0x$log_addr"
 	sleep 0.5
